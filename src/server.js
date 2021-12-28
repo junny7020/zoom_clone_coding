6bc9a4bb633f99ad2fc1737084879adeced16f5a
June Choi <junny7020@gmail.com>
2021-12-28 19:55:34 +0000

    28th Dec 2021 chapter 1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,24 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
