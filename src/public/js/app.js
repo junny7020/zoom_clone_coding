bd7b7dc07c54e5494657ace5a879c49bd3abd7dc
June Choi <junny7020@gmail.com>
2022-03-23 18:29:09 +0000

    #2.7 coding challenge initial commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,18 +2,64 @@ const socket = io();
 
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = "";
+}
+
+function handleNIcknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+}
+
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    nameForm.addEventListener("submit", handleNIcknameSubmit);
+    msgForm.addEventListener("submit", handleMessageSubmit);
+}
 
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = form.querySelector('input');
-    socket.emit("enter_room", {payload: input.value}, () => {
-        console.log("server is done!");
-    });
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
 
+socket.on("welcome", (user) => {
+    addMessage(`${user} has joined!`);
+});
+
+socket.on("bye", (left) => {
+    addMessage(`${left} has left`);
+});
+
+socket.on("new_message", addMessage);
 // const messageList = document.querySelector("ul");
 // const messageForm = document.querySelector("#message");
 // const nickForm = document.querySelector("#nick");
